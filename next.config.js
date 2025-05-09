105bea0c84e3e88823d07daf151820e7258ce9bd
spider076 <saadabban76@gmail.com>
2025-05-09 16:19:34 +0000

    removed console.logs in production via next.config file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        unoptimized: true,
-    }
-}
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    // this runs only in the producttion builds
+    if (!dev) {
+      // the terser plugin handles minification/console.log service
+      const terserPluginIndex = config.optimization.minimizer.findIndex(
+        (minimizer) => minimizer.constructor.name === "TerserPlugin"
+      );
 
-module.exports = nextConfig
+      if (terserPluginIndex > -1) {
+        const terserPlugin = config.optimization.minimizer[terserPluginIndex];
+
+        terserPlugin.options.terserOptions = {
+          ...terserPlugin.options.terserOptions,
+          compress: {
+            ...terserPlugin.options.terserOptions?.compress,
+            drop_console: true,
+          },
+        };
+      }
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- providers/PHProvider.tsx
+++ providers/PHProvider.tsx
@@ -4,9 +4,6 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
-  console.log("posthey key : ", process.env.NEXT_PUBLIC_POSTHOG_KEY);
-  console.log("posthey host : ", process.env.NEXT_PUBLIC_POSTHOG_HOST);
-
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     capture_pageview: false, // Disable automatic pageview capture, as we capture manually
